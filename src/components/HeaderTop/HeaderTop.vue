eb009350022828d0199cd99dd06bc013c03ef1c7
lc <644231692@qq.com>
2020-12-07 15:21:44 +0800

    1

---
+++ src/components/HeaderTop/HeaderTop.vue
@@ -0,0 +1,67 @@
+<template>
+  <header class="header">
+
+    <slot name="left"/>
+    <span class="header_title">
+      <span class="header_title_text ellipsis">{{ title }}</span>
+    </span>
+    <slot name="right"/>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  },
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.header
+  width 100%
+  height 45px
+  position fixed
+  left 0
+  right 0
+  top 0
+  z-index 100
+  background #02a774
+
+  .header_search
+    position absolute
+    left 15px
+    top 50%
+    transform translateY(-50%)
+    width 10%
+    height 50%
+
+    .icon-sousuo
+      font-size 25px
+      color #fff
+
+  .header_title
+    position absolute
+    left 50%
+    top 50%
+    transform translate(-50%, -50%)
+    width 50%
+    color #fff
+    text-align center
+
+    .header_title_text
+      font-size 20px
+      color #fff
+      display block
+
+  .header_login
+    position absolute
+    right 15px
+    top 50%
+    transform translateY(-50%)
+    color #fff
+    font-size 14px
+
+    .header_login_text
+      color #fff
+</style>
---
+++ src/pages/Msite/Msite.vue
@@ -0,0 +1,213 @@
+<template>
+  <div class="msite">
+    <HeaderTop :title="'aaa'">
+      <router-link to="/search" slot="left" class="header_search">
+        <i class="iconfont icon-sousuo"/>
+      </router-link>
+      <router-link to="/search" slot="right" class="header_login">
+        <span class="header_login_text">
+           登录|注册
+        </span>
+      </router-link>
+    </HeaderTop>
+    <div class="miste-content-wrapper">
+      <div class="miste-content">
+        <nav class="msite_nav ">
+          <div class="swiper-container">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">
+
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/1.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/2.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/3.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/4.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/5.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/6.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/7.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/8.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+              </div>
+              <div class="swiper-slide">
+
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/1.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/2.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/3.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/4.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/5.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/6.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/7.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+                <a href="javascript:" class="link_to_food">
+                  <div class="food_container">
+                    <img src="./images/nav/8.jpg">
+                  </div>
+                  <span>甜品</span>
+                </a>
+              </div>
+            </div>
+            <div class="swiper-pagination"></div>
+          </div>
+
+        </nav>
+      </div>
+    </div>
+    <div class="msite_shop_list">
+      <div class="shop_header">
+        <i class="iconfont icon-xuanxiang"></i>
+        <span class="shop_header_title">附近商家</span>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import HeaderTop from "../../components/HeaderTop/HeaderTop";
+import Swiper from 'swiper'
+import 'swiper/dist/css/swiper.min.css'
+
+export default {
+  components: {
+    HeaderTop
+  },
+  mounted() {
+    new Swiper('.swiper-container', {
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination'
+      }
+    })
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import "../../common/stylus/mixins.styl"
+.msite
+  width 100%
+  .miste-content-wrapper
+    position fixed
+    top 45px
+    bottom 46px
+    width 100%
+
+    .msite_nav
+      bottom-border-1px(#e4e4e4)
+      height 200px
+      background #fff
+
+      .swiper-container
+        width 100%
+        height 100%
+
+        .swiper-wrapper
+          width 100%
+          height 100%
+
+          .swiper-slide
+            display flex
+            justify-content center
+            align-items flex-start
+            flex-wrap wrap
+
+            .link_to_food
+              width 25%
+
+              .food_container
+                display block
+                width 100%
+                text-align center
+                padding-bottom 10px
+                font-size 0
+
+                img
+                  display inline-block
+                  width 50px
+                  height 50px
+
+              span
+                display block
+                font-size 13px
+                width 100%
+                text-align center
+                color #666
+
+        .swiper-pagination
+          &.swiper-pagination-bullet-active
+            background #02a774
+
+
+</style>
