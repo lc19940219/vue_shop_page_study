4cb6470c796a8c1ca0ee15ff34e43a7cdbbd396a
lc <644231692@qq.com>
2020-12-16 22:27:18 +0800

    1

--- src/components/ShopHeader/ShopHeader.vue
+++ src/components/ShopHeader/ShopHeader.vue
@@ -17,10 +17,10 @@
         </h2>
         <div class="shop-message">
           <span class="shop-message-detail">{{ info.score }}</span>
-          <span class="shop-message-detail">月售{{info.sellCount}}单</span>
+          <span class="shop-message-detail">月售{{ info.sellCount }}单</span>
           <span class="shop-message-detail">
-           {{info.description}}
-            <span>约{{info.deliveryTime}}分钟</span>
+           {{ info.description }}
+            <span>约{{ info.deliveryTime }}分钟</span>
           </span>
           <span class="shop-message-detail">距离{{ info.distance }}</span>
         </div>
@@ -32,11 +32,11 @@
           <span class="content-tag">
             <span class="mini-tag">{{ info.supports[0].name }}</span>
           </span>
-          <span class="activity-content ellipsis">{{info.supports[0].content}}</span>
+          <span class="activity-content ellipsis">{{ info.supports[0].content }}</span>
         </div>
       </div>
       <div class="discounts-right">
-        {{info.supports.length}}个优惠
+        {{ info.supports.length }}个优惠
       </div>
 
     </div>
@@ -71,7 +71,7 @@
             <span>公告</span>
           </h3>
           <div class="brief-modal-notice">
-            {{info.bulletin}}
+            {{ info.bulletin }}
           </div>
           <div class="mask-footer">
             <span class="iconfont icon-close"></span>
@@ -87,36 +87,18 @@
             优惠活动
           </h2>
           <ul class="list">
-            <li class="activity-item">
-              <span class="cotent-tag">
+            <li class="activity-item"
+                v-for="(support,index) in info.supports" :key="index" :class="supportClasses[support.type]">
+              <span class="content-tag">
                 <span class="mini-tag">
-                  xxx
-                </span>
-                <span class="activity-content ">
-                  xxxx
+                  {{ support.name }}
                 </span>
               </span>
+              <span class="activity-content ">
+                  {{ support.content }}
+                </span>
             </li>
-            <li class="activity-item">
-              <span class="cotent-tag">
-                <span class="mini-tag">
-                  xxx
-                </span>
-                <span class="activity-content ">
-                  xxxx
-                </span>
-              </span>
-            </li>
-            <li class="activity-item">
-              <span class="cotent-tag">
-                <span class="mini-tag">
-                  xxx
-                </span>
-                <span class="activity-content ">
-                  xxxx
-                </span>
-              </span>
-            </li>
+
           </ul>
           <div class="activity-sheet-close">
             <span class="iconfont icon-close"></span>
@@ -132,14 +114,15 @@
 
 <script>
 import {mapState} from 'vuex'
+
 export default {
-  data(){
+  data() {
     return {
       supportClasses: ['activity-green', 'activity-red', 'activity-orange'],
 
     }
   },
-  computed:{
+  computed: {
     ...mapState(['info'])
   }
 }
@@ -153,6 +136,7 @@ export default {
   color #fff
   overflow hidden
   position relative
+
   .shop_nav
     background-size cover
     background-repeat no-repeat
@@ -168,34 +152,40 @@ export default {
     //  border-bottom 0
     //  background red
     //  background-color rgba(119, 103, 137, .43)
+
     .back
       position absolute
       left 0
       top 10px
+
       .icon-arrow_left
         display block
         padding 5px
         font-size 20px
         color #fff
+
   .shop-content
     padding 30px 20px 5px 20px
     display flex
     background #fff
     position relative
     text-align center
+
     img
-      box-shadow 0 0 0.4vw 0 rgba(0,0,0,0.2);
+      box-shadow 0 0 0.4vw 0 rgba(0, 0, 0, 0.2);
       position absolute
       top 0
       left 50%
       width 66px
-      height:66px
+      height: 66px
       border-radius 2px
       margin-left -33px
       margin-top -40px
+
     .header-content
       width 72%
       flex 1
+
       .content-title
         font-size 20px
         color #333
@@ -205,17 +195,19 @@ export default {
         align-items center
         justify-content center
         display flex
+
         .content-tag
-          background-image linear-gradient(90deg,#fff100,#ffe339)
+          background-image linear-gradient(90deg, #fff100, #ffe339)
           border-radius 2px
-          width:36px
-          height:18px
+          width: 36px
+          height: 18px
           color #6a3709
 
           margin-right 10px
           font-style normal
           font-weight 700
           position relative
+
           .mini-tag
             position absolute
             left 0
@@ -229,34 +221,37 @@ export default {
             bootom -100%
             font-weight 600
             transform-origin 0 0
+
         .content-name
           font-weight 700
           text-align left
           overflow hidden
           text-overflow ellipsis
+
         .content-icon
-          width:20px
-          height:16px
+          width: 20px
+          height: 16px
           position relative
+
           &::after
             content ""
             position absolute
             left 8px
             top 3px
             border 6px solid
-            border-color  transparent transparent transparent  rgba(0,0,0,0.67)
+            border-color transparent transparent transparent rgba(0, 0, 0, 0.67)
+
       .shop-message
         height 12px
         white-space nowrap
         margin-top 8px
         font-size 11px
         color #333
+
         .shop-message-detail:not(:last-child)::after
           content '\B7'
           opacity 0.2
           margin-left 1px
-
-
 
 
   .shop-header-discounts
@@ -269,22 +264,26 @@ export default {
     color #666
     margin 0 30px
     align-items center
+
     .discounts-left
       flex 1
       overflow hidden
+
       .activity
         display flex
         align-items center
+
         .content-tag
           border-radius 1px
-          width:25px
-          height:13px
+          width: 25px
+          height: 13px
           margin-right 5px
           color #fff
           font-style normal
           font-weight 700
           position relative
-          background-color rgb(112,188,70)
+          background-color rgb(112, 188, 70)
+
           .mini-tag
             position absolute
             left 0
@@ -298,12 +297,14 @@ export default {
             display flex
             align-items center
             justify-content center
+
     .discounts-right
-      width:50px
+      width: 50px
       flex-shrink 0
       padding-right 10px
       text-align right
       position relative
+
       &::after
         content ""
         position absolute
@@ -328,18 +329,22 @@ export default {
     z-index 52
     flex-direction column
     color #333
-    &.fade-enter-activity,&.fade-leave-activity
+
+    &.fade-enter-activity, &.fade-leave-activity
       transform opacity 0.5s
-    &.fade-enter,&.fade-leave-to
+
+    &.fade-enter, &.fade-leave-to
       opacity 0
+
     .biref-model-cover
       position absolute
-      background-color rgba(0,0,0,0.5)
-      width:100%
-      height:100%
+      background-color rgba(0, 0, 0, 0.5)
+      width: 100%
+      height: 100%
       top 0
       left 0
       z-index 1
+
     .brief-model-content
       width 80%
       padding 25px 20px
@@ -349,6 +354,7 @@ export default {
       z-index 99
       position relative
       flex-direction column
+
       .content-title
         font-size 20px
         line-height 24px
@@ -357,17 +363,20 @@ export default {
         display flex
         align-items center
         justify-content center
-        >span
+
+        > span
           font-weight 600
+
         .content-tag
           border-radius 2px
-          background-image linear-gradient(90deg,#fff100,#ffe339);
+          background-image linear-gradient(90deg, #fff100, #ffe339);
           width 36px
           height 18px
           font-style normal
           margin-right 10px
           color #6a3709
           position relative
+
           .mini-tag
             position absolute
             top 0
@@ -386,17 +395,20 @@ export default {
         display flex
         margin 20px -10px 0
 
-        >li
+        > li
           flex 1
           text-align center
-          >h3
+
+          > h3
             font-size 15px
             font-weight 600
             color #333
             margin-bottom 8px
-          >p
+
+          > p
             font-size 12px
             color #999
+
       .biref-model-title
         text-align center
         margin 15px auto 15px
@@ -405,16 +417,19 @@ export default {
         background-size 100% 1px
         background-repeat no-repeat
         background-position 50%
-        >span
+
+        > span
           background-color #fff
           font-size 12px
           padding 0 6px
           color #999
+
       .brief-modal-notice
         font-size 13px
         line-height 1.54
         color #333
         overflow-y auto
+
       .mask-footer
         position absolute
         bottom -60px
@@ -423,22 +438,93 @@ export default {
         border-radius 50%
         border 1px solid rgba(255, 255, 255, .7)
         transform translateX(-50%)
+
         span
           color #fff
           font-size 16px
-
-
 
 
   .activity-sheet
     position fixed
     top 0
     left 0
-    width:100%
-    height:100%
+    width: 100%
+    height: 100%
     z-index 99
+
     .activity-sheet-content
+      background-color #f5f5f5
+      box-shadow 0 -1px 5px 0 rgba(0, 0, 0, 0.4)
+      bottom 0
+      left 0
+      right 0
+      z-index 100
+      padding 20px 30px
+      box-sizing border-box
+      color #333
+      position absolute
+
+      .activity-sheet-title
+        text-align center
+        font-size 20px
+        font-weight 600
+        margin-bottom 20px
+
+      .list
+        font-size 16px
+        height 160px
+        overflow-y auto
+
+        .activity-item
+          margin-bottom 12px
+          display flex
+          font-size 13px
+          align-items center
+
+          &.activity-green
+            .content-tag
+              background-color rgb(112, 188, 70)
+
+          &.activity-red
+            .content-tag
+              background-color rgb(240, 115, 115)
+
+          &.activity-orange
+            .content-tag
+              background-color: rgb(241, 136, 79)
+
+          .content-tag
+            display inline-block
+            border-radius 2px
+            width 36px
+            height: 18px
+            margin-right 10px
+            color #fff
+            font-style normal
+            position relative
+
+            .mini-tag
+              position absolute
+              left 0
+              top 0
+              right -100%
+              bottom -100%
+              font-size 24px
+              transform scale(.5)
+              transform-origin 0 0
+              display flex
+              align-items center
+              justify-content center
 
 
+      .activity-sheet-close
+        position absolute
+        right 6px
+        top 10px
+        width 25px
+        height 25px
+
+        > span
+          font-size 20px
 
 </style>
